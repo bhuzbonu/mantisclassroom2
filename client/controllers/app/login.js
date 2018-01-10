c99a992bf7e8b00472bb7854080a8dc61e93b208
Zoltan Bonus <zoltan.bonus@flex.com>
2018-01-10 00:20:23 -0800

    [Extension] Update node "password", Create screen "Login", Create node "submit", Update node "submit"

---
+++ client/controllers/app/login.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_login', app_login);
+function app_login($scope, app) {
+    'use strict';
+    app.init($scope);
+}
---
+++ client/mocks/login.js
@@ -0,0 +1,3 @@
+'use strict';
+window.mocks = window.mocks || {};
+window.mocks.login = window.mocks.login || {};
